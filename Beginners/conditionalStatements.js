11cfb0d470d0a6603686c8b69b948094248c9fec
Richa <richa.parekh@pavansgroup.in>
2024-12-30 17:19:40 +0530

    Code update

--- Beginners/conditionalStatements.js
+++ Beginners/conditionalStatements.js
@@ -1,16 +1,23 @@
-const num = 0
-console.log("// else if conditional statement")
+let num = 0
+console.log("// if conditional statement")
+if(num === 0){
+    console.log('Number is zero') // Output: Number is zero
+}
+
+console.log("// if-else conditional statement")
+num = -10;
 if(num > 0){
     console.log('Number is positive')
 }else{
-    console.log('Number is negative')
+    console.log('Number is negative') // Output: Number is negative
 }
 
 console.log("// else if conditional statement")
+num = 0;
 if(num > 0){
-    console.log('Number is positive')
-}else if (num < 0){
-    console.log('Number is negative')
+    console.log('Number is positive') 
+}else if (num <= 0){
+    console.log('Number is negative') // Output: Number is negative
 }else {
     console.log('Number is zero')
 }
@@ -19,7 +26,7 @@ console.log("// switch conditional statement")
 const color = 'red'
 switch(color){
     case 'red': 
-        console.log("Color is red")
+        console.log("Color is red") // Output: Color is red
         break
     case 'blue': 
         console.log("Color is blue")
@@ -29,4 +36,22 @@ switch(color){
         break
     default:
         console.log("Not a valid color")
+}
+
+num = 20
+console.log("// Ternary Operator")
+let result = num >= 0 ? "Number is positive" : "Number is negative";
+console.log(result) // Output: Number is positive
+
+console.log("// Nested if-else Statement")
+let operation = "+";
+
+if (num >= 0) {
+  if (operation === "+") {
+    console.log("Sum of number is " + (num + 100)); // Output: Sum of number is 120
+  } else {
+    console.log("Invalid choice");
+  }
+} else {
+  console.log("Negative values not allowed");
 }
--- Beginners/equality.js
+++ Beginners/equality.js
@@ -1,41 +1,41 @@
 console.log("== Case 1 ==")
 const var1 = '10'
 const var2 = '10'
-console.log(var1 == var2)
-console.log(var1 === var2)
+console.log(var1 == var2) // Output: true
+console.log(var1 === var2) // Output: true
 
 console.log("== Case 2 ==")
 const var3 = '10'
 const var4 = 10
-console.log(var3 == var4) // JavaScript automatically coercion variable var4 to var3 string type
-console.log(var3 === var4) // Must ensure both value have same datatype
+console.log(var3 == var4) // Output: true // JavaScript automatically coercion variable var4 to var3 string type
+console.log(var3 === var4) // Output: false // Must ensure both value have same datatype
 
 console.log("== Case 3 ==")
 const var5 = ''
 const var6 = 10
-console.log(var5 == var6)
-console.log(var5 === var6)
+console.log(var5 == var6) // Output: false
+console.log(var5 === var6) // Output: false
 
 console.log("== Case 4 ==")
 const var7 = ''
 const var8 = 10
-console.log(var7 == var8)
-console.log(var7 === var8)
+console.log(var7 == var8) // Output: false
+console.log(var7 === var8) // Output: false
 
 console.log("== Case 5 ==")
 const var9 = 0
 const var10 = ''
-console.log(var9 == var10)
-console.log(var9 === var10)
+console.log(var9 == var10) // Output: true
+console.log(var9 === var10) // Output: false
 
 console.log("== Case 6 ==")
 const var11 = false
 const var12 = ''
-console.log(var11 == var12)
-console.log(var11 === var12)
+console.log(var11 == var12) // Output: true
+console.log(var11 === var12) // Output: false
 
 console.log("== Case 7 ==")
 const var13 = null
 const var14 = undefined
-console.log(var13 == var14)
-console.log(var13 === var14)
+console.log(var13 == var14) // Output: true
+console.log(var13 === var14) // Output: false
