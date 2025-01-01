caf489dae9304c2c975b22e042c6b3c2d5427970
Richa <richa.parekh@pavansgroup.in>
2025-01-01 16:57:20 +0530

    Update looping.js

--- Beginners/looping.js
+++ Beginners/looping.js
@@ -2,6 +2,12 @@ console.log("== For loop ==")
 for(let var1 = 1; var1 <= 5; var1++){
     console.log('Iteration number '+ var1)
 }
+/* Output:
+Iteration number 1
+Iteration number 2
+Iteration number 3
+Iteration number 4
+Iteration number 5 */
 
 console.log("== While loop ==")
 let var2 = 1
@@ -9,6 +15,12 @@ while(var2 <= 5){
     console.log('Iteration number '+ var2)
     var2++
 }
+/* Output:
+Iteration number 1
+Iteration number 2
+Iteration number 3
+Iteration number 4
+Iteration number 5 */
 
 console.log("== Do while loop :: Example 1 ==")
 let var3 = 1
@@ -16,6 +28,12 @@ do {
     console.log('Iteration number '+ var3)
     var3++
 }while(var3 <= 5)
+/* Output:
+Iteration number 1
+Iteration number 2
+Iteration number 3
+Iteration number 4
+Iteration number 5 */
 
 console.log("== Do while loop :: Example 2 ==")
 let var4 = 6
@@ -23,6 +41,8 @@ do {
     console.log('Iteration number '+ var4)
     var4++
 }while(var4 <= 5)
+/* Output:
+Iteration number 6 */
 
 console.log("== For..of loop ==")
 const numArray = [1,2,3,4,5]
@@ -30,3 +50,37 @@ const numArray = [1,2,3,4,5]
 for(const num of numArray){
     console.log('Iteration number '+ num)
 }
+/* Output:
+Iteration number 1
+Iteration number 2
+Iteration number 3
+Iteration number 4
+Iteration number 5 */
+console.log("== For...in loop ==")
+const user = { name: "Richa", age: 25, role: "Developer" };
+for (let key in user) {
+    console.log(`${key}: ${user[key]}`);
+}
+/* Output:
+name: Richa
+age: 25
+role: Developer */
+console.log("== break ==")
+for (let i = 1; i <= 5; i++) {
+    if (i === 3) break;
+    console.log("Iteration number "+i);
+}
+/* Output:
+Iteration number 1
+Iteration number 2 */
+
+console.log("== continue ==")
+for (let i = 1; i <= 5; i++) {
+    if (i === 3) continue;
+    console.log("Iteration number "+i);
+}
+/* Output:
+Iteration number 1
+Iteration number 2
+Iteration number 4
+Iteration number 5 */
