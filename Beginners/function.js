0107be4d2caf4e67453bcf2bb9622cd6bba7ddd4
Richa <richa.parekh@pavansgroup.in>
2025-01-02 17:54:09 +0530

    Update function.js file

--- Beginners/function.js
+++ Beginners/function.js
@@ -1,15 +1,25 @@
 function greet(username){
     console.log("Hello! Good morning " + username)
 }
-greet('Richa')
+greet('Richa') 
 greet('Mike')
 greet('Aly')
+/* Output:
+Hello! Good morning Richa
+Hello! Good morning Mike
+Hello! Good morning Aly */
 
-function add(var1, var2){
-    return var1 + var2
+console.log("// Function Declaration ")
+function add(a, b) {
+    return a + b;
 }
-const result1 = add(10, 5)
-console.log(result1)
+console.log(add(5, 3)); // Output: 8
+
+console.log("// Function Expression ")
+const multiply = function(a, b) {
+    return a * b;
+};
+console.log(multiply(4, 2)); // Output: 8
 
 console.log("// Arrow functions :: Introduce in 2015 ")
 const addNumber = (a, b) => {
@@ -27,3 +37,26 @@ console.log(result3)
 const divideNumber = (num) => num / 2
 const result4 = divideNumber(20)
 console.log(result4)
+
+console.log("// Anonymous Functions ")
+setTimeout(function() {
+    console.log("This message appears after 2 seconds");
+}, 2000);
+
+console.log("// Callback Functions ")
+function num(n, callback) {
+    return callback(n);
+}
+const double = (n) => n * 2;
+console.log(num(5, double)); // Output: 10
+
+console.log("// Nested Functions ")
+function outerFun(a) {
+    function innerFun(b) {
+        return a + b;
+    }
+    return innerFun;
+}
+
+const addTen = outerFun(10);
+console.log(addTen(5)); // Output: 15
